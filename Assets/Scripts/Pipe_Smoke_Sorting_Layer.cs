ebc80d0663bee14468e3f6575cad8f1566d7f981
Dave Levy <slightshift@gmail.com>
2015-01-24 21:49:32 +0200

    Added inhalation system and some texts

---
+++ Assets/Scripts/Pipe_Smoke_Sorting_Layer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe_Smoke_Sorting_Layer : MonoBehaviour {
+		void Start ()
+		{
+			// Set the sorting layer of the particle system.
+			particleSystem.renderer.sortingLayerName = "Pipe_Smoke";
+			//particleSystem.renderer.sortingOrder = 0;
+	
+	}
+}
