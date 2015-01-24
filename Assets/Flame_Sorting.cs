019acb78d867726c103aa2fb2149be246392b456
Dave Levy <slightshift@gmail.com>
2015-01-24 14:34:52 +0200

    Made flame sprite

---
+++ Assets/Flame_Sorting.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flame_Sorting : MonoBehaviour {
+
+	void Start () {
+		renderer.sortingLayerName = "Flame";
+		Debug.Log ("Sorting layer is:" + renderer.sortingLayerName);
+	}
+}
