ff05e1558cc2eefa4585434c02a88f14185165b8
Dave Levy <slightshift@gmail.com>
2015-01-24 19:29:05 +0200

    Added speech bubble and puffticles

---
+++ Assets/Scripts/Smoke_Sorting_Layer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smoke_Sorting_Layer : MonoBehaviour {
+		void Start ()
+		{
+			// Set the sorting layer of the particle system.
+			particleSystem.renderer.sortingLayerName = "Smoke";
+			//particleSystem.renderer.sortingOrder = 0;
+	
+	}
+}
