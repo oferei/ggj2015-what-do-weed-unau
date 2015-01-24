e9e894f00c2f0502bf7010445ff291cf213d0d03
Ofer Reichman <oferei@gmail.com>
2015-01-24 11:01:58 +0200

    scene art update from Dave.

---
+++ Assets/Set_FG_Leave_Sorting.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Set_FG_Leave_Sorting : MonoBehaviour {
+		void Start ()
+		{
+			// Set the sorting layer of the particle system.
+			particleSystem.renderer.sortingLayerName = "FG_Leaves";
+			//particleSystem.renderer.sortingOrder = 0;
+	
+	}
+}
